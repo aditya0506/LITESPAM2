657e4471f7d9643e922ebcc96b17c21c25de0950
aditya0506 <41143270+aditya0506@users.noreply.github.com>
2018-07-12 15:21:12 +0700

    SPAM HAHA
    Jangan di perjual belikan

---
+++ 4.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'bomphd.php';
+
+/*
+
+https://github.com/nee48/BomSmsPhD
+
+Created by Aditya Ramdhani
+
+Modified by まやちゃん
+
+*/
+
+$init = new Bom();
+
+//Eksekusi Sms Boomber
+
+echo "Nomor Target (pakai 62): ";
+
+$a = trim(fgets(STDIN));
+
+$init->no = "$a";
+
+echo "Jumlah Pesan: ";
+
+$b = trim(fgets(STDIN));
+
+$loop = "$b";
+
+for ($i=0; $i < $loop; $i++) {
+
+$init->Verif($init->no);
+
+}
+
