f1d4e48c9ce8d243301784892c3d8840beeeabd3
aditya0506 <41143270+aditya0506@users.noreply.github.com>
2018-07-12 17:27:34 +0700

    Add files via upload

---
+++ LITESPAM.sh
@@ -0,0 +1,216 @@
+#!/system/xbin/bash
+
+clear
+
+toilet -f slant --gay "LiteSpam"
+
+sleep 1
+
+echo "\033[31;1m Sedikit tools untuk spam"
+
+sleep 1
+
+echo "\033[33;1m Author: Aditya Ramdhani"
+
+sleep 2
+
+echo "\033[32;1m ✈contac : 089601377646"
+
+sleep 1
+
+echo "\033[34;1m ✈Indonesia Cyber Prime✈"
+
+sleep 1
+
+echo "\033[36;1m Spam Yang Tersedia :"
+
+echo "1. Tokopedia"
+
+echo "2. Telkomsel"
+
+echo "3. Matahari Mall"
+
+echo "4. PHD"
+
+echo "5. Jd.Id"
+
+echo "6. Email Bomber"
+
+echo "0. Keluar"
+
+echo "\033[30;1m81. Informasi Tool Ini"
+
+echo "\033[33;1m Pilih Angka:"
+
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+
+then
+
+clear
+
+echo "\033[34;1m"
+
+figlet "tokopedia"
+
+php 1.php
+
+fi
+
+
+if
+
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+
+then
+
+clear
+
+echo "\033[31;1m"
+
+toilet "T-Sel"
+
+php 2.php
+
+fi
+
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+
+then
+
+clear
+
+echo "\033[31;1m"
+
+figlet "Mthr Mall"
+
+php 3.php
+
+fi
+
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+
+then
+
+clear
+
+toilet -f mono12 -F gay "PHD"
+
+echo "\033[30;1m"
+
+php 4.php
+
+fi
+
+
+if
+
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+
+then
+
+clear
+
+toilet -f mono12 -F gay "Jd.Id"
+
+echo "\033[33;1m"
+
+php 5.php
+
+fi
+
+
+if
+
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+
+then
+
+clear
+
+toilet -f standard -F gay "Email Bomber"
+
+echo "\033[36;1m"
+
+python2 6.py
+
+fi
+
+
+if
+
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+
+then
+
+clear
+
+toilet -f slant --gay "LiteSpam"
+
+echo "\033[31;1mNama tools: LiteSpam"
+
+sleep 1
+
+echo "\033[33;1mKarya: Aditya Ramdhani (V3 By Mr.Xi'Anzing)"
+
+sleep 1
+
+echo "\033[32;1mVersi: v3"
+
+sleep 1
+
+echo "\033[36;1mTeam: ✈Indonesia Cyber Prime✈"
+
+sleep 1
+
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://youtube.com/IndonesiaSecurityLite"
+
+echo "\033[31;1m Spesial Thanks To: "
+
+echo "\033[36;1m"
+
+echo "Aditya Ramdhani"
+
+echo "Rizki Maulana"
+
+echo "まやちゃん"
+
+echo "Baby Cyber Team"
+
+echo "SBG-Team"
+
+echo "Ponorogo Defacer Team"
+
+sleep 1
+
+echo "And All Member Indonesia Cyber Prime"
+
+echo "\033[30;1m tunggu 8 detik"
+
+sleep 8
+
+sh LITESPAM.sh
+
+fi
+
+
+if
+
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+
+then
+
+echo "\033[31;1m Keluar"
+
+sleep 1
+
+echo "\033[32;1m Sampai berjumpa lagi :)"
+
+sleep 1
+
+fi
+
